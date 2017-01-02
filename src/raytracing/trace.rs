24d9e216f122005a2f53a843e499055b46eb03d9
Kyle Stachowicz <kylestach99@gmail.com>
2017-01-01 18:01:37 -0800

    Add tracing logic

--- src/raytracing/trace.rs
+++ src/raytracing/trace.rs
@@ -1,4 +1,5 @@
 extern crate nalgebra as na;
+extern crate image;
 use raytracing::*;
 
 #[derive(Clone)]
@@ -11,4 +12,42 @@ pub struct Intersection {
 pub trait Trace {
     fn intersect(&self, ray: Ray) -> Option<Intersection>;
     fn material<'a>(&'a self) -> &'a Box<Material>;
+}
+
+struct IntersectedObject<'a> {
+    intersection: Intersection,
+    object: &'a Box<Trace>,
+}
+
+pub fn trace<'a>(scene: &'a Scene, eye: Ray) -> image::Rgb<u8> {
+    let best = scene.objects
+        .iter()
+        .fold::<Option<IntersectedObject>, _>(None, |intersected_object, new_obj| {
+            match (intersected_object, new_obj.intersect(eye.clone())) {
+                (Some(intersected_object), Some(intersection)) => {
+                    if intersected_object.intersection.hit_distance > intersection.hit_distance {
+                        Some(IntersectedObject {
+                            intersection: intersection,
+                            object: &new_obj,
+                        })
+                    } else {
+                        Some(intersected_object)
+                    }
+                }
+                (None, Some(intersection)) => {
+                    Some(IntersectedObject {
+                        intersection: intersection,
+                        object: &new_obj,
+                    })
+                }
+                (Some(intersected_object), None) => Some(intersected_object),
+                (None, None) => None,
+            }
+        });
+
+    if let Some(IntersectedObject { intersection, object }) = best {
+        object.material().surface(intersection, scene)
+    } else {
+        image::Rgb([0u8, 0u8, 0u8])
+    }
 }
